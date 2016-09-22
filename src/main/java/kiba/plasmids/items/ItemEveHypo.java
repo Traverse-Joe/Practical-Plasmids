5f5ba1a1f03d6f9337825a4ce2046800279b9455
Unknown <roxxanrebirth@gmail.com>
2016-09-22 03:31:38 -0400

    -Fixed the Eve Hypo's to Use Item on Use

--- src/main/java/kiba/plasmids/items/ItemEveHypo.java
+++ src/main/java/kiba/plasmids/items/ItemEveHypo.java
@@ -5,6 +5,7 @@ import kiba.plasmids.energy.IEveHolder;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
@@ -13,7 +14,7 @@ public class ItemEveHypo extends BaseItem {
     public ItemEveHypo() {
         super("eve_Hypo");
         this.setMaxStackSize(8);
-        this.setMaxDamage(1);
+
 
     }
 
@@ -21,7 +22,10 @@ public class ItemEveHypo extends BaseItem {
     public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
         IEveHolder holder = playerIn.getCapability(PlasmidsCapabilities.EVE_HOLDER, null);
         holder.givePower(100, false);
-        //Need to Make Item Get Used Up on Use
+        playerIn.attackEntityFrom(DamageSource.generic, 0.1F);
+        --itemStackIn.stackSize;
         return new ActionResult(EnumActionResult.SUCCESS, itemStackIn);
-    }
+         }
+
 }
+
