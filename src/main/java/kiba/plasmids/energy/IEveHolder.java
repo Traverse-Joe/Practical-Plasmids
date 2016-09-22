5693034843fe0157ac22d7e45efe0d7124f58bbd
TheCodedOne <dr.kcodex@gmail.com>
2016-09-22 03:10:29 +0100

    fixy fixy

--- src/main/java/kiba/plasmids/energy/IEveHolder.java
+++ src/main/java/kiba/plasmids/energy/IEveHolder.java
@@ -1,8 +1,14 @@
 package kiba.plasmids.energy;
 
+import net.minecraft.nbt.NBTTagCompound;
+
 public interface IEveHolder {
 
     long getStoredPower();
 
     long getCapacity();
+
+    void deserializeNBT(NBTTagCompound nbt);
+
+    NBTTagCompound serializeNBT();
 }
