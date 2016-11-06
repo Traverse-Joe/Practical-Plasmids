19228d7f60fb3f5963d464bcadce46d7378442e3
Kiba Belmont <roxxanrebirth@gmail.com>
2016-11-06 00:19:45 -0400

    Added API

---
+++ src/main/java/kiba/plasmids/api/package-info.java
@@ -0,0 +1,5 @@
+@API(apiVersion = Globals.VERSION, owner = Globals.MODID, provides = "plasmids|API")
+package kiba.plasmids.api;
+
+import kiba.plasmids.Globals;
+import net.minecraftforge.fml.common.API;
