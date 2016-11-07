b3f2f919fced785d65eae18931cadca692534a9d
Kiba Belmont <roxxanrebirth@gmail.com>
2016-11-07 02:34:49 -0500

    Fixed Crash with Protein Hypos (renamed Protein Vials

--- src/main/java/kiba/plasmids/items/ItemFoodVial.java
+++ src/main/java/kiba/plasmids/items/ItemFoodVial.java
@@ -2,11 +2,9 @@ package kiba.plasmids.items;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
 
@@ -36,17 +34,19 @@ public class ItemFoodVial extends BaseItem {
 
     @Override
     public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving) {
+
         if (entityLiving instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) entityLiving;
             player.attackEntityFrom(DamageSource.generic, 0.1F);
             player.getFoodStats().setFoodLevel(20);
-            player.getFoodStats().setFoodSaturationLevel(5.0F);
+            player.getFoodStats().addStats(20, 5f);
             worldIn.playSound((EntityPlayer) null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
             --stack.stackSize;
         }
+
+
         return stack;
     }
-
 
 
 }
