1a183c5b5d932a805cf7f029083a41a939f44093
UpcraftLP <j-schirra@gmx.de>
2017-08-26 01:49:04 +0200

    added annotations to api package-info file

--- src/main/java/kiba/plasmids/api/package-info.java
+++ src/main/java/kiba/plasmids/api/package-info.java
@@ -1,5 +1,10 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 @API(apiVersion = Globals.VERSION, owner = Globals.MODID, provides = "plasmids|API")
 package kiba.plasmids.api;
 
 import kiba.plasmids.Globals;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraftforge.fml.common.API;
+
+import javax.annotation.ParametersAreNonnullByDefault;
