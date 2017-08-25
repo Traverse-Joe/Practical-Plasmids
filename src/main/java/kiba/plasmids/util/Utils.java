edb19deccd5a57ef6a43e8f36e1f3e9cd75478f3
UpcraftLP <j-schirra@gmx.de>
2017-08-26 01:49:09 +0200

    added util class

---
+++ src/main/java/kiba/plasmids/util/Utils.java
@@ -0,0 +1,29 @@
+package kiba.plasmids.util;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+
+/**
+ * @author UpcraftLP
+ */
+public class Utils {
+
+    /**
+     * Spawns particles
+     */
+    public static void spawnParticles(Entity entity, EnumParticleTypes particleType, int numberOfParticles, double xPos, double yPos, double zPos, double xOffset, double yOffset, double zOffset, double speed) {
+        spawnParticles(entity.world, particleType, numberOfParticles, xPos, yPos, zPos, xOffset, yOffset, zOffset, speed);
+    }
+
+    /**
+     * Spawns particles
+     */
+    public static void spawnParticles(World world, EnumParticleTypes particleType, int numberOfParticles, double xPos, double yPos, double zPos, double xOffset, double yOffset, double zOffset, double speed) {
+        if(world instanceof WorldServer) {
+            ((WorldServer) world).spawnParticle(particleType, xPos, yPos, zPos, numberOfParticles, xOffset, yOffset, zOffset, speed);
+        }
+        else if(world.isRemote) for(int i = 0; i < numberOfParticles; i++) world.spawnParticle(particleType, particleType.getShouldIgnoreRange(), xPos + world.rand.nextDouble() * xOffset, yPos + world.rand.nextDouble() * yOffset, zPos + world.rand.nextDouble() * zOffset, world.rand.nextDouble() * speed, world.rand.nextDouble() * speed, world.rand.nextDouble() * speed);
+    }
+}
